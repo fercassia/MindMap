249c9a805124e01a0f3a6b6bf9829799d9715cae
Fernanda Gomes <feh.cmrg@outlook.com>
2025-01-03 17:32:04 -0300

    Delete LINGUAGENS/NODE directory

--- LINGUAGENS/NODE/Stream/streams/transform-stream.js
+++
@@ -1,9 +0,0 @@
-import { Transform } from 'node:stream';
-
-export default class InverseNumberStream extends Transform{
-  _transform(chunk, encoding, callback) {
-    const transformedNumber = Number(chunk.toString())*-1;
-
-    callback(null, Buffer.from(transformedNumber.toString()));
-  }
-}
--- LINGUAGENS/NODE/Stream/streams/writable-stream.js
+++
@@ -1,8 +0,0 @@
-import { Writable } from 'node:stream'
-
-export default class MultiplyByTenStream extends Writable {
-  _write(chunk, encoding, callback) {
-    console.log(Number(chunk.toString())*10)
-    callback()
-  }
-}
